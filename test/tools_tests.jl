ec5955b373ace5bbfd659dddf506e6200eca075a
Rodri <rgruiz0001@gmail.com>
2022-08-30 12:37:36 +0200

    update tests for significance

--- test/tools_tests.jl
+++ test/tools_tests.jl
@@ -2,6 +2,6 @@ using HistDist
 using Test
 
 @testset "significance" begin
-@test significance([1,1,1],[1,1,1]) == 0
-@test significance([1,1],[1,1,1]) == 0
+@test significance([1,1,1],[1,1,1],1) == 0
+@test significance([1,1],[1,1,1],1) == 0
 end
